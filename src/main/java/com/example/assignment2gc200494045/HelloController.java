cf955b5419e111fbd64e9a4b879459890f0a94c0
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2022-11-15 20:21:52 -0800

    Assignment2

---
+++ src/main/java/com/example/assignment2gc200494045/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.assignment2gc200494045;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment2gc200494045 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment2gc200494045 to javafx.fxml;
+    exports com.example.assignment2gc200494045;
+}
