b750bc71b55e6fa3fff0c2886438d8720df91600
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2022-11-26 19:03:51 -0800

    Assignment2-  Added the style.css for the first view and added an icon

--- src/main/java/com/example/assignment2gc200494045/Main.java
+++ src/main/java/com/example/assignment2gc200494045/Main.java
@@ -3,6 +3,7 @@ package com.example.assignment2gc200494045;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Yuchen's Book Database!");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("images/bookicon.png")));
         stage.setScene(scene);
         stage.show();
     }
