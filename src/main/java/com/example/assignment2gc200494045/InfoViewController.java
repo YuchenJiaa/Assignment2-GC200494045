3c6ab22e3a6968671ce7ec0c8c37e0069b84e4f8
YuchenJiaa <113528470+YuchenJiaa@users.noreply.github.com>
2022-11-27 11:38:42 -0800

    Assignment2-  Added a view to show the details

---
+++ src/main/java/com/example/assignment2gc200494045/InfoViewController.java
@@ -0,0 +1,11 @@
+package com.example.assignment2gc200494045;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class InfoViewController {
+
+    @FXML
+    private Label moreInfoLabel;
+
+}
